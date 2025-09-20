ff2581d8428999246c80698c54776e699288e83b
juanPrad0 <pradogarciajuandavid@gmail.com>
2025-09-19 21:00:37 -0600

    Reto

---
+++ Reto/Ventana.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Ventana extends JFrame {
+    JButton boton;
+    Mensajes controlador;
+    Punto primero;
+    Punto segundo;
+    Punto tercero;
+    
+    public Ventana(Mensajes controlador,Punto primero,Punto segundo,Punto tercero) {
+        final double EPSILON = 1e-6;
+        this.controlador = controlador;
+        this.primero = primero;
+        this.segundo = segundo;
+        this.tercero = tercero;
+    
+
+
+        Double distancia1y2 = Math.sqrt(Math.pow(segundo.x-primero.x, 2)+Math.pow(segundo.y-primero.y, 2));
+        Double distancia2y3 = Math.sqrt(Math.pow(tercero.x-segundo.x, 2)+Math.pow(tercero.y-segundo.y, 2));
+        Double distancia1y3 = Math.sqrt(Math.pow(tercero.x-primero.x, 2)+Math.pow(tercero.y-primero.y, 2));
+        //String equilatero = (distancia1y2==distancia1y3 && distancia1y2==distancia2y3)?"Si es equilatero":"No es equilatero";
+        String equilatero = 
+        (Math.abs(distancia1y2 - distancia1y3) < EPSILON &&
+        Math.abs(distancia1y2 - distancia2y3) < EPSILON)
+        ? "Si es equilatero" 
+        : "No es equilatero";
+
+        setTitle("Triangulo Equilatero");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        boton = new JButton("Click para resultados");
+        boton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, 
+                controlador.mensajePuntoA() + primero.toString() + "\n" + 
+                controlador.mensajePuntoB() + segundo.toString() + "\n" + 
+                controlador.mensajePuntoC() + tercero.toString() + "\n" + 
+                controlador.mensajeDistancia1y2() + distancia1y2 + "\n" + 
+                controlador.mensajeDistancia2y3() + distancia2y3 + "\n" + 
+                controlador.mensajeDistancia1y3() + distancia1y3 + "\n" +
+                equilatero
+                );
+            }
+        });
+
+        add(boton);
+    }
+}
