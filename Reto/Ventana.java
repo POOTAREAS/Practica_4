e8dc7a8ff3a49405c9df29205e3a73448ca6e36b
sxid <92390610+sxidhub@users.noreply.github.com>
2025-09-19 21:51:28 -0600

    Update Ventana.java
    Quitamos EPSILON

--- Reto/Ventana.java
+++ Reto/Ventana.java
@@ -8,8 +8,7 @@ public class Ventana extends JFrame {
     Punto segundo;
     Punto tercero;
     
-    public Ventana(Mensajes controlador,Punto primero,Punto segundo,Punto tercero) {
-        final double EPSILON = 1e-6;
+    public Ventana(Mensajes controlador,Punto primero,Punto segundo,Punto tercero) {;
         this.controlador = controlador;
         this.primero = primero;
         this.segundo = segundo;
@@ -22,8 +21,8 @@ public class Ventana extends JFrame {
         Double distancia1y3 = Math.sqrt(Math.pow(tercero.x-primero.x, 2)+Math.pow(tercero.y-primero.y, 2));
         //String equilatero = (distancia1y2==distancia1y3 && distancia1y2==distancia2y3)?"Si es equilatero":"No es equilatero";
         String equilatero = 
-        (Math.abs(distancia1y2 - distancia1y3) < EPSILON &&
-        Math.abs(distancia1y2 - distancia2y3) < EPSILON)
+        (Math.abs(distancia1y2 - distancia1y3) < 0.0001 &&
+        Math.abs(distancia1y2 - distancia2y3) < 0.0001 )
         ? "Si es equilatero" 
         : "No es equilatero";
 
@@ -40,9 +39,9 @@ public class Ventana extends JFrame {
                 controlador.mensajePuntoA() + primero.toString() + "\n" + 
                 controlador.mensajePuntoB() + segundo.toString() + "\n" + 
                 controlador.mensajePuntoC() + tercero.toString() + "\n" + 
-                controlador.mensajeDistancia1y2() + distancia1y2 + "\n" + 
-                controlador.mensajeDistancia2y3() + distancia2y3 + "\n" + 
-                controlador.mensajeDistancia1y3() + distancia1y3 + "\n" +
+                controlador.mensajeDistancia1y2() + String.format("%.3f", distancia1y2) + "\n" + 
+                controlador.mensajeDistancia2y3() + String.format("%.3f", distancia2y3) + "\n" + 
+                controlador.mensajeDistancia1y3() + String.format("%.3f", distancia1y3) + "\n" +
                 equilatero
                 );
             }
